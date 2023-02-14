9f753adde6ca7530dd3dbc18b6ad672e543bb9d7
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-02-14 16:12:42 -0500

    ADD: refactor main into Main class

--- lib/main.rb
+++ lib/main.rb
@@ -7,41 +7,58 @@ require_relative './parser'
 require_relative './turn'
 require_relative './board'
 
-reader = Reader.new
-selector = Selector.new
-parser = Parser.new
-collector = Collector.new
-turn = Turn.new
-
-txt = reader.read
-
-words = parser.parse(txt)
-words = collector.collect(words)
-word = selector.select(words)
-
-board = Board.new(word)
-  
-def take_a_guess
-  print 'guess: '
-  guess = gets.chomp.downcase
-  return guess
-end
-
-def errored_out?
-  board.errors != 7
-end
-
-def game_over?
-  errored_out? && board.is_word_unsolved?
-end
-
-while board.is_word_unsolved?
-  puts ''
-  puts "guesses: #{board.wrong_guesses} word: #{board.board.join}  turn: #{turn.turn} errors: #{board.errors}/7"
-  guess = take_a_guess
-  if board.is_guess_in_word?(guess)
-    board.update_board(guess)
+class Main
+  def initialize(
+    reader = Reader.new,
+    selector = Selector.new,
+    parser = Parser.new,
+    collector = Collector.new,
+    turn = Turn.new,
+    board = nil
+  )
+    @reader = reader
+    @selector = selector
+    @parser = parser
+    @collector = collector
+    @turn = turn
+    @board = nil
   end
-  board.add_guess(guess)
-  turn.increment_turn
+
+  def setup_board
+    txt = @reader.read
+    words = @parser.parse(txt)
+    words = @collector.collect(words)
+    word = @selector.select(words)
+    @board = Board.new(word)
+  end
+
+  def take_a_guess
+    print 'guess: '
+    guess = gets.chomp.downcase
+  end
+  
+  def errored_out?
+    @board.errors != 7
+  end
+  
+  def game_not_over?
+    errored_out? && @board.is_word_unsolved?
+  end
+
+  def start_game
+    while game_not_over?
+      puts ''
+      puts "guesses: #{@board.wrong_guesses} word: #{@board.board.join}  turn: #{@turn.turn} errors: #{@board.errors}/7"
+      guess = take_a_guess
+      if @board.is_guess_in_word?(guess)
+        @board.update_board(guess)
+      end
+      @board.add_guess(guess)
+      @turn.increment_turn
+    end
+  end
 end
+
+game  = Main.new
+game.setup_board
+game.start_game
