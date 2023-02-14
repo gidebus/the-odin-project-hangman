ddfbbee0ff43cc18d7bcfc90431795dea85791d1
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-02-14 13:13:10 -0500

    ADD: functional basic iteration of game

--- lib/main.rb
+++ lib/main.rb
@@ -16,4 +16,39 @@ words = parser.parse(txt)
 words = collector.collect(words)
 word = selector.select(words)
 
-p word
+
+word_size = word.length
+word = word.split('')
+fake_word =  Array.new(word_size, '.')
+  
+def take_a_guess
+  puts 'guess:'
+  guess = gets.chomp
+  return guess
+end
+
+  # if word.include?(guess)
+  #   word.each_with_index do |char, index|
+  #     if char == guess
+  #       fake_word[index] = char
+  #     end
+  #   end
+  #   p fake_word
+  # end
+
+while true
+  if fake_word.include?('.')
+    guess = take_a_guess
+    if word.include?(guess)
+      word.each_with_index do |char, index|
+        if char == guess
+          fake_word[index] = char
+        end
+      end
+    end
+  else
+    break
+  end
+  p word
+  p fake_word
+end
